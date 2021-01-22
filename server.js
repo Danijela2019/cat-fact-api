0472839610c82d361eeb811fdde6cb787235232b
Danijela2019 <arsic_danijela@yahoo.com>
2021-01-22 14:06:52 +0100

    Use mongoose to connect to db

--- server.js
+++ server.js
@@ -1,11 +1,24 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const routes = require('./routes')
+const mongoose = require('mongoose');
 
 const server = express();
 const port = process.env.PORT || 5000;
 
 server.use(bodyParser.json());
+
+mongoose.connect('mongodb://localhost:27017/sandbox');
+
+const db = mongoose.connection;
+db.on('error', (err) => {
+    console.error('Connection error happened', err)
+})
+db.once('open', ()=>{
+    console.log('Db connection OPEN');
+
+});
+
 server.use('/api/v1/catfacts',routes);
 
 server.use((_req, _res, next) => {
